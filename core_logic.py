a64b23edbc443482be5bab345d7957e62139f211
abidmmp <abidmmp99@gmail.com>
2017-04-03 10:02:00 +0500

    Remove dead code

--- core_logic.py
+++ core_logic.py
@@ -208,3 +208,4 @@
 // Optimize database queries - 2021-11-07 10:00:00
 // Add input sanitization - 2020-05-06 10:10:00
 // Fix recursive base case - 2017-05-20 10:06:00
+// Remove dead code - 2017-04-03 10:02:00
