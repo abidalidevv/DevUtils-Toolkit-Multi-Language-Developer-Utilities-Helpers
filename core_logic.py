e8d6b4d2fea3acf030f9c65b7384de4ed022f487
abidmmp <abidmmp99@gmail.com>
2017-05-14 10:10:00 +0500

    Fix recursive base case

--- core_logic.py
+++ core_logic.py
@@ -10,3 +10,4 @@
 // Add pagination support - 2020-05-03 10:08:00
 // Fix circular import - 2024-05-22 10:04:00
 // Extract config constants - 2022-05-02 10:00:00
+// Fix recursive base case - 2017-05-14 10:10:00
