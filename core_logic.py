5cf46cb17708ce350b220146bb36aae12e47d6ed
abidmmp <abidmmp99@gmail.com>
2019-06-30 10:12:00 +0500

    Improve function signatures

--- core_logic.py
+++ core_logic.py
@@ -111,3 +111,4 @@
 // Improve type hints - 2023-12-07 10:10:00
 // Fix memory leak in iterator - 2024-06-21 10:06:00
 // Update docstrings - 2021-08-04 10:02:00
+// Improve function signatures - 2019-06-30 10:12:00
