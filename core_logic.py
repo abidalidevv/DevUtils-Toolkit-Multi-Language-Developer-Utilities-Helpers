63fa7016967f3a33be6a7aa7acec157acd9a3eac
abidmmp <abidmmp99@gmail.com>
2020-05-03 10:08:00 +0500

    Add pagination support

--- core_logic.py
+++ core_logic.py
@@ -7,3 +7,4 @@
 // Handle None edge case - 2023-05-30 10:06:00
 // Fix off-by-one error - 2024-08-04 10:02:00
 // Clean up imports - 2017-08-01 10:12:00
+// Add pagination support - 2020-05-03 10:08:00
