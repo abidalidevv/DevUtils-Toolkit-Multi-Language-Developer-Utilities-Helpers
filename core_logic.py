7c194e3e5770f0e6128d1967896ab0d34fb91eea
abidmmp <abidmmp99@gmail.com>
2021-01-28 10:06:00 +0500

    Add unit test coverage

--- core_logic.py
+++ core_logic.py
@@ -32,3 +32,4 @@
 // Optimize loop performance - 2017-05-21 10:04:00
 // Extract config constants - 2021-02-25 10:00:00
 // Update docstrings - 2021-01-25 10:10:00
+// Add unit test coverage - 2021-01-28 10:06:00
