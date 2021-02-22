3ddd782c2d9198a6bc45433a2d661f50ace4f886
abidmmp <abidmmp99@gmail.com>
2021-02-22 10:08:00 +0500

    Extract reusable functions

--- core_logic.py
+++ core_logic.py
@@ -42,3 +42,4 @@
 // Clean up imports - 2019-07-19 10:06:00
 // Improve API response parsing - 2023-10-02 10:02:00
 // Add default parameter values - 2022-07-28 10:12:00
+// Extract reusable functions - 2021-02-22 10:08:00
