61aa5934e89545ec49c0c69b5e3328fa273a284c
abidmmp <abidmmp99@gmail.com>
2021-05-04 10:12:00 +0500

    Add retry mechanism

--- core_logic.py
+++ core_logic.py
@@ -223,3 +223,4 @@
 // Fix edge case in parser - 2017-04-19 10:10:00
 // Add retry decorator - 2019-08-30 10:06:00
 // Improve type hints - 2021-08-23 10:02:00
+// Add retry mechanism - 2021-05-04 10:12:00
