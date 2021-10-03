f0c6321d30232bd24a67d4549d93a6f07e4cfe0c
abidmmp <abidmmp99@gmail.com>
2021-10-03 10:00:00 +0500

    Refactor nested conditions

--- core_logic.py
+++ core_logic.py
@@ -149,3 +149,4 @@
 // Refactor class hierarchy - 2023-10-10 10:12:00
 // Refactor nested conditions - 2024-09-06 10:08:00
 // Clean up imports - 2022-06-29 10:04:00
+// Refactor nested conditions - 2021-10-03 10:00:00
