a4e0fe2e5f4ecb75e00dbc4b46b8214b59f8eaf4
abidmmp <abidmmp99@gmail.com>
2021-10-27 10:12:00 +0500

    Refactor nested conditions

--- core_logic.py
+++ core_logic.py
@@ -251,3 +251,4 @@
 // Add helper methods - 2023-04-09 10:10:00
 // Extract reusable functions - 2021-06-14 10:06:00
 // Update docstrings - 2023-03-22 10:02:00
+// Refactor nested conditions - 2021-10-27 10:12:00
