0dd358c72c5e90b45f9f95c2e383a08b74371d54
abidmmp <abidmmp99@gmail.com>
2022-03-26 10:04:00 +0500

    Fix off-by-one error

--- core_logic.py
+++ core_logic.py
@@ -106,3 +106,4 @@
 // Add input sanitization - 2019-06-01 10:02:00
 // Clean up unused variables - 2017-05-17 10:12:00
 // Improve API response parsing - 2018-05-02 10:08:00
+// Fix off-by-one error - 2022-03-26 10:04:00
