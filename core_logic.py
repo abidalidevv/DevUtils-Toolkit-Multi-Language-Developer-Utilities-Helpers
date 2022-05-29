b097a8eefdf48a163f957de9a90a5ea8faaeb1b3
abidmmp <abidmmp99@gmail.com>
2022-05-29 10:02:00 +0500

    Add type annotations

--- core_logic.py
+++ core_logic.py
@@ -187,3 +187,4 @@
 // Add helper methods - 2024-10-19 10:00:00
 // Handle empty list edge case - 2020-02-17 10:10:00
 // Fix attribute access bug - 2020-01-24 10:06:00
+// Add type annotations - 2022-05-29 10:02:00
