f1ed20dff650cde5646ded9ac0239a1e0d5bbeb8
abidmmp <abidmmp99@gmail.com>
2022-08-20 10:00:00 +0500

    Refactor utility functions

--- core_logic.py
+++ core_logic.py
@@ -163,3 +163,4 @@
 // Clean up imports - 2024-08-14 10:12:00
 // Improve exception messages - 2019-05-18 10:08:00
 // Extract config constants - 2023-10-26 10:04:00
+// Refactor utility functions - 2022-08-20 10:00:00
