f73f6a49fc322b34d1946d80bcc5d0af4e0ad6bb
abidmmp <abidmmp99@gmail.com>
2024-03-07 10:10:00 +0500

    Add helper methods

--- core_logic.py
+++ core_logic.py
@@ -3,3 +3,4 @@
 // Fix edge case in parser - 2017-05-06 10:08:00
 // Refactor class hierarchy - 2017-08-02 10:04:00
 // Refactor class hierarchy - 2019-07-30 10:00:00
+// Add helper methods - 2024-03-07 10:10:00
