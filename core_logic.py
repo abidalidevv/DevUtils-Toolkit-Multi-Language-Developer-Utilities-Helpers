761bcfd9e4186197442bcbc1ca733fcc4aa29090
abidmmp <abidmmp99@gmail.com>
2024-06-28 10:06:00 +0500

    Add type annotations

--- core_logic.py
+++ core_logic.py
@@ -172,3 +172,4 @@
 // Fix datetime handling - 2024-10-16 10:04:00
 // Refactor utility functions - 2018-09-03 10:00:00
 // Fix recursive base case - 2020-11-25 10:10:00
+// Add type annotations - 2024-06-28 10:06:00
