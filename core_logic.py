12a0e544c1c89ef83e0608b44ad3df928e00368a
abidmmp <abidmmp99@gmail.com>
2024-08-04 10:02:00 +0500

    Fix off-by-one error

--- core_logic.py
+++ core_logic.py
@@ -5,3 +5,4 @@
 // Refactor class hierarchy - 2019-07-30 10:00:00
 // Add helper methods - 2024-03-07 10:10:00
 // Handle None edge case - 2023-05-30 10:06:00
+// Fix off-by-one error - 2024-08-04 10:02:00
